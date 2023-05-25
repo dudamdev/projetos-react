edffd8fc842654c817943256f15e330e92cefe7b
dudamdev <duda.mdev@gmail.com>
2023-05-25 15:14:25 -0300

    Requisição do catálogo e produto por id do backend com Axios

--- dscommerce-frontend/src/main.tsx
+++ dscommerce-frontend/src/main.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // Ferramenta para avisar de possíveis problemas na aplicação
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 )
--- dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -4,10 +4,23 @@ import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductCard from "../../../components/ProductCard";
 import * as productService from "../../../services/product-service"
 import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
+import axios from "axios";
 
 export default function ProductDetails() {
-    const params = useParams()
-    const product = productService.findById(Number(params.productId))
+    const params = useParams();
+
+    const [product, setProduct] = useState<ProductDTO>();
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/products/${params.productId}`)
+            .then(response => {
+                console.log(response.data);
+                setProduct(response.data)
+            })
+    }, []);
+
     return (
         <main>
             <section id="product-details-section" className="dsc-container">
